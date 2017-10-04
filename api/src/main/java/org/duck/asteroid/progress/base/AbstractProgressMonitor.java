02a7265ea7c674a38a50c0eb834107c0e54eb191
Chris Senior <chris.senior@snapon.com>
2017-10-04 13:50:51 +0100

    Radically new API

--- api/src/main/java/org/duck/asteroid/progress/base/AbstractProgressMonitor.java
+++ api/src/main/java/org/duck/asteroid/progress/base/AbstractProgressMonitor.java
@@ -1,139 +1,83 @@
 package org.duck.asteroid.progress.base;
 
-import java.util.ArrayList;
-
+import org.duck.asteroid.progress.FractionalProgress;
 import org.duck.asteroid.progress.ProgressMonitor;
-import org.duck.asteroid.progress.ProgressMonitorListener;
-import org.duck.asteroid.progress.ProgressMonitorListener.EventType;
+import org.duck.asteroid.progress.base.format.ProgressFormat;
+import org.duck.asteroid.progress.base.frac.AbstractFractionalProgress;
+import org.duck.asteroid.progress.base.frac.FractionalProgressInteger;
+import org.duck.asteroid.progress.base.frac.FractionalProgressLong;
+
+import java.util.concurrent.ConcurrentLinkedDeque;
 
 /**
- * An abstract base class for all monitors.
+ * An abstract base class for all monitors. This handles listeners status and name.
  */
 public abstract class AbstractProgressMonitor implements ProgressMonitor {
 
-	/** The total work being tracked */
-	protected int totalWork = 100;
-	/** The work done (so far) */
-	protected int workDone = 0;
 	/** The current task name */
-	protected String taskName = "";
+	protected final String taskName;
 	/** The last set value of {@link #notify(String)} */
 	protected String status = "";
-	/** A list of current listeners */
-	protected ArrayList<ProgressMonitorListener> listeners = new ArrayList<ProgressMonitorListener>(1);
+	/** A list of the fractional projections of this progress */
+	protected ConcurrentLinkedDeque<AbstractFractionalProgress<?>> projections = new ConcurrentLinkedDeque<>();
 
 	public AbstractProgressMonitor() {
-		super();
+		this.taskName = "";
 	}
 
-	public void begin(int total) {
-		this.totalWork = total;
-		this.workDone = 0;
-		notify(EventType.BEGIN);
-		logUpdate(this);
+	public AbstractProgressMonitor(final String name) {
+		this.taskName = name;
 	}
 
-	public int getTotalWork() {
-		return totalWork;
-	}
-
-	public int getWorkDone() {
-		return workDone;
-	}
-
-	public void setWorkDone(int done) {
-		int worked = done - workDone;
-		if (done > 0) {
-			int work = Math.min(worked, getWorkRemaining());
-			worked(work, null);
-		}
-	}
-
-	public int getWorkRemaining() {
-		return totalWork - workDone;
-	}
-
-	public void setWorkRemaining(int remaining) {
-		if (remaining >= 0 && remaining <= getTotalWork()) {
-			setWorkDone(totalWork - remaining);
-		}
-	}
-
-	public void done() {
-		worked(getWorkRemaining(), null);
-	}
-
-	/**
-	 * This is the main work update method.
-	 * Both {@link #setWorkDone(int)} and {@link #setWorkRemaining(int)} call this
-	 */
-	public synchronized void worked(int work, String status) {
-		if (work > 0) {
-			workDone += work;
-			if (status != null) {
-				this.status = status;
-			}
-			notify(EventType.WORK_DONE);
-			logUpdate(this);
-			checkCompleted();
-		}
-	}
-
-	private void checkCompleted() {
-		if (isWorkComplete()) {
-			notify(EventType.COMPLETED);
-		}
-	}
-
-	public boolean isWorkComplete() {
-		return workDone == totalWork; 
-	}
-
-	public double getFractionDone() {
-		return (double)workDone / (double)totalWork;
-	}
-
+	@Override
 	public String getTaskName() {
 		return taskName;
 	}
-	
+
+	@Override
 	public String getStatus() {
 		return status;
 	}
-	
+
+	@Override
 	public void notify(String status) {
 		this.status = status;
-		notify(EventType.NOTIFICATION);
 		logUpdate(this);
 	}
-	
-	public ProgressMonitor newSubTask(int work, String taskName) {
-		return new SubTaskProgressMonitor(this, work, taskName);
+
+	@Override
+	public ProgressMonitor split(double work, String taskName) {
+		SubTaskProgressMonitor subTask = new SubTaskProgressMonitor(this, work, taskName);
+		return subTask;
 	}
-	
-	public synchronized void addUpdateListener(ProgressMonitorListener listener) {
-		listeners.add(listener);
+
+	@Override
+	public FractionalProgress<Integer> asInteger(int total) {
+		FractionalProgressInteger progressInteger = new FractionalProgressInteger(this, total);
+		projections.add(progressInteger);
+		return progressInteger;
 	}
-	
-	public synchronized void removeUpdateListener(ProgressMonitorListener listener) {
-		listeners.remove(listener);
+
+	@Override
+	public FractionalProgress<Long> asLong(long total) {
+		FractionalProgressLong progressLong = new FractionalProgressLong(this, total);
+		projections.add(progressLong);
+		return progressLong;
 	}
-	
+
 	/**
-	 * Notify current listeners of an event in this monitor
-	 * @param type The type of event
+	 * A mechanism for projections and core progress to communicate updates without
+	 * circularity
+	 * @param fractionDone the fraction done
+	 * @param ignored the source of the internal update (if any) - will not be notified
 	 */
-	protected synchronized void notify(ProgressMonitorListener.EventType type) {
-		ArrayList<ProgressMonitorListener> tmp = new ArrayList<ProgressMonitorListener>(listeners);
-		for(ProgressMonitorListener listener : tmp) {
-			listener.progressUpdated(type, this);
-		}
-	}
+	public abstract void setFractionDoneInternal(final double fractionDone, AbstractProgressMonitor ignored);
+
 	/**
-	 * called whenever some udpate needs to be logged to UI in the chain of monitors
-	 * @param child The child that raised the log
+	 * A method for the root monitor to hook into updates in the tree
+	 * @param source the source of the update
 	 */
-	protected abstract void logUpdate(AbstractProgressMonitor child);
+	public abstract void logUpdate(ProgressMonitor source);
 
 	@Override
 	public String toString() {
